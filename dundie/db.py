e02e398a374aefe526472b56a530a4057ced1957
lianker <lianker7@gmail.com>
2025-01-08 21:29:37 -0300

    Create User endpoints
    - define serializers
    - Dependency injection
    - add user views
    - create and set routes to user endpoints

--- dundie/db.py
+++ dundie/db.py
@@ -1,5 +1,6 @@
 """Database connection"""
-from sqlmodel import create_engine
+from fastapi import Depends
+from sqlmodel import create_engine, Session
 from .config import settings
 
 engine = create_engine(
@@ -7,3 +8,10 @@ engine = create_engine(
     echo=settings.db.echo,  # pyright: ignore
     connect_args=settings.db.connect_args,  # pyright: ignore
 )
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+ActiveSession = Depends(get_session)
